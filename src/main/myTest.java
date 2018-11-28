e58d8e992963346fb9bffa3fc7411d4c2b5e8cf6
NegarMirgati <negar.nbj95@gmail.com>
2018-11-29 00:27:36 +0330

    Add project test class

---
+++ src/main/myTest.java
@@ -0,0 +1,17 @@
+import ast.VisitorImpl;
+import ast.node.Program;
+import org.antlr.v4.runtime.*;
+import java.io.IOException;
+
+public class myTest {
+    public static void main(String[] args) throws IOException {
+        CharStream reader = CharStreams.fromFileName(args[0]);
+        testLexer lexer = new testLexer(reader);   // SmoolaLexer in your project
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        testParser parser = new testParser(tokens);   // SmoolaParser in your project
+        Program p = parser.program().p; // program is the name of the start rule
+
+        VisitorImpl v = new VisitorImpl();
+        p.accept(v);
+    }
+}
